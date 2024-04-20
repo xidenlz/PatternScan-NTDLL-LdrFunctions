9f2307c097149e33799ad1f452ed0b64b365ca09
Musaed <106308342+Mes2d@users.noreply.github.com>
2024-04-20 15:52:09 -0700

    Update pScanNtdll.cpp

--- src/pScanNtdll/pScanNtdll.cpp
+++ src/pScanNtdll/pScanNtdll.cpp
@@ -40,8 +40,29 @@ int main() {
     const char* LdrpLoadDllInternal = "\x8B\x03\x89\x44\x24\x28"; // 8B 03 89 44 24 28 does not accepts wilcards I'm not sure
     const char* LdrpLoadDllInternal_Mask = "xxxxxx";
 
+    /* LdrpLoadDll */
+    const char* LdrpLoadDll = "\x40\x55\x53\x56\x57\x41\x56\x41\x57\x48\x8D\x6C\x24\x88"; // 40 55 53 56 57 41 56 41 57 48 8D 6C 24 88 does not accepts wilcards I'm not sure
+    const char* LdrpLoadDll_Mask = "xxxxxx";
+
+
+    /* LdrLoad DLL*/
     uintptr_t LdrLoadDll_Addr = FindPattern("ntdll.dll", LdrLoadDll, LdrLoadDll_Mask);
+
+    /* LdrpLoadInternalDll */
     uintptr_t LdrpLoadDllInternal_Addr = FindPattern("ntdll.dll", LdrpLoadDllInternal, LdrpLoadDllInternal_Mask);
+
+
+    /* LdrpLoadDll */
+    uintptr_t LdrpLoadDll_Addr = FindPattern("ntdll.dll", LdrpLoadDll, LdrpLoadDll_Mask);
+
+    if (LdrpLoadDll_Addr)
+    {
+        std::cout << "LdrpLoadDll found at address: 0x" << std::hex << LdrpLoadDll_Addr << std::endl;
+    }
+    else
+    {
+        std::cout << "LdrpLoadDll not found." << std::endl;
+    }
 
     if (LdrLoadDll_Addr) {
         std::cout << "LdrLoadDll found at address: 0x" << std::hex << LdrLoadDll_Addr << std::endl;
@@ -57,7 +78,5 @@ int main() {
         std::cout << "LdrpLoadDllInternal not found." << std::endl;
     }
 
-    printf("End\n");
     system("pause");
-    //return 0;
 }
